546ca6135de48ceba4a0065cf6cc609d3305cfc6
MacBook Air 13 <ror.web_developer@yahoo.com>
2021-01-27 18:10:42 +0300

    Added controller Users and user registration page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  get 'signup' => 'users#new'
   get 'help' => 'static_pages#help'
   get 'contacts' => 'static_pages#contacts'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
