4489fb108a67b73c05a9bd6d6ac730281a514db2
MacBook Air 13 <ror.web_developer@yahoo.com>
2021-01-28 21:03:15 +0300

    Added routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
   get 'help' => 'static_pages#help'
   get 'contacts' => 'static_pages#contacts'
+
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
