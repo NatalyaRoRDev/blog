075cecad0bcb92f37bb42479e9b35fb51dfeb849
MacBook Air 13 <ror.web_developer@yahoo.com>
2021-01-28 20:15:50 +0300

    Added password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_sequre_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
