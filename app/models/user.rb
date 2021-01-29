4e3cc9da983ce0acdec5b5133a43f9ac02d61bb3
MacBook Air 13 <ror.web_developer@yahoo.com>
2021-01-29 18:13:55 +0300

    Added has_secure_password to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
+  has_secure_password
 
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
