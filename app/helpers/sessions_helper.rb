fc565ba5a5aa1408c1a7ba997adfd56755bce1e5
MacBook Air 13 <ror.web_developer@yahoo.com>
2021-01-30 18:55:48 +0300

    Returns the current user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,4 +3,9 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # Возвращает текущего вошедшего пользователя (если есть).
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
