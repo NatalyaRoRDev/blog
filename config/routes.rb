8c5991d3461869b930b65cb97420c7d62c8a97a4
MacBook Air 13 <ror.web_developer@yahoo.com>
2021-01-30 12:23:03 +0300

    Added controller sessions and routes for login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
   get 'signup' => 'users#new'
   get 'help' => 'static_pages#help'
   get 'contacts' => 'static_pages#contacts'
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
