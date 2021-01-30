3ad69d50e791bb4d1706bbdfb375e473cbd48eb9
MacBook Air 13 <ror.web_developer@yahoo.com>
2021-01-30 16:27:05 +0300

    log_in method and user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,10 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Осуществить вход пользователя и перенаправление на страницу профиля.
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Неверный email или пароль'
-      # Выдать сообщение об ошибке.
       render 'new'
     end
   end
