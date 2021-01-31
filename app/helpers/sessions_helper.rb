f854965a8eb9bf88ce4134c9bd5aacd738bfe537
MacBook Air 13 <ror.web_developer@yahoo.com>
2021-01-31 21:50:56 +0300

    Added logout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,4 +13,10 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+
+  # Осуществляет выход текущего пользователя.
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
