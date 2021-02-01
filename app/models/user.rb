264d5c285b07fc16caed2dca2f691518c048f388
MacBook Air 13 <ror.web_developer@yahoo.com>
2021-02-01 14:53:07 +0300

    For safety

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
   has_secure_password
 
@@ -9,4 +11,23 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   
   validates :password, presence: true, length: { minimum: 6 }
+
+  # Возвращает дайджест данной строки.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Возвращает случайный токен
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Запоминает пользователя в базе данных для использования в постоянной сессии.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
 end
