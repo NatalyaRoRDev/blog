4f64c2d586c75d232c55aa54002c198aa54cafb5
MacBook Air 13 <ror.web_developer@yahoo.com>
2021-02-01 17:07:36 +0300

    Permanent login session

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,4 +30,9 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+  # Возвращает true, если предоставленный токен совпадает с дайджестом.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
 end
