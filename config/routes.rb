91f942bd50b55f4a5dbc666d62a326761aa02bb7
MacBook Air 13 <ror.web_developer@yahoo.com>
2021-02-07 12:58:51 +0300

    Created AccountActivations controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   delete 'logout' => 'sessions#destroy'
 
   resources :users
+  resources :account_activations, only: [:adit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
