e79ab9ba886a915137dd4937d0f3471db9e411ed
MacBook Air 13 <ror.web_developer@yahoo.com>
2021-02-07 12:35:31 +0300

    Added several users including admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,25 @@
+User.create!(name:  "Наталья",
+             age: '34',
+             city: 'Moscow',
+             email: "ruby_developer1@yahoo.com",
+             password:              "Lina96MyFriend$",
+             password_confirmation: "Lina96MyFriend$",
+             admin: true)
+
+5.times do |n|
+  name  = Faker::Name.name
+  age = '25'
+  city = 'Moskow'
+  email = "example-#{n+1}@gmail.com"
+  password = "password"
+  User.create!(name:  name,
+               age: age,
+               city: city,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
