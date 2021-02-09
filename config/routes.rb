9cca523546d61a67315febcb0a851cad11667b9b
MacBook Air 13 <ror.web_developer@yahoo.com>
2021-02-09 20:53:53 +0300

    Added columns to the user table and some styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'sessions/new'
   root 'static_pages#home'
   get 'signup' => 'users#new'
@@ -11,5 +13,6 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
