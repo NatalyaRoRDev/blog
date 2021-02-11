49d8f139dc7ea16d5b33ccc6d9bd8bf8a79ac4d7
MacBook Air 13 <ror.web_developer@yahoo.com>
2021-02-11 12:50:51 +0300

    Create action to reset password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,19 @@ class PasswordResetsController < ApplicationController
   def new
   end
 
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "Электронное письмо с инструкциями по сбросу пароля отправлено"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "Данный адрес электронной почты не найден"
+      render 'new'
+    end
+  end
+
   def edit
   end
 end
