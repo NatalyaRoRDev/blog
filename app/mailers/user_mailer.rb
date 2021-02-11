f3dfac92a694e5a5774b50c5d098f9477c843688
MacBook Air 13 <ror.web_developer@yahoo.com>
2021-02-11 16:17:12 +0300

    Bug fixes for password change

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -16,14 +16,4 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: "Сброс пароля"
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.password_reset.subject
-  #
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -61,7 +61,7 @@ class User < ApplicationRecord
   def create_reset_digest
     self.reset_token = User.new_token
     update_attribute(:reset_digest, User.digest(reset_token))
-    update_attribute(reset_sent_at, Time.zone.now)
+    update_attribute(:reset_sent_at, Time.zone.now)
   end
 
   # Отправляет электронное письмо для сброса пароля
