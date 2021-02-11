7d3959df73b627c62eee1655a5f908c700c833c0
MacBook Air 13 <ror.web_developer@yahoo.com>
2021-02-11 21:03:32 +0300

    Password change form

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -7,7 +7,8 @@ class PasswordResetsController < ApplicationController
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
-      flash[:info] = "Электронное письмо с инструкциями по сбросу пароля отправлено"
+      flash[:info] = "Электронное письмо с инструкциями 
+      по сбросу пароля отправлено на вашу электронную почту"
       redirect_to root_url
     else
       flash.now[:danger] = "Данный адрес электронной почты не найден"
