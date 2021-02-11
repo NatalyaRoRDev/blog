b364eadf77cf21d6aa401f997047a7faf3db6bbb
MacBook Air 13 <ror.web_developer@yahoo.com>
2021-02-11 13:00:31 +0300

    Created method that sets the attributes for password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
 
@@ -55,6 +55,13 @@ class User < ApplicationRecord
   # Отправляет активационное письмо
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # Устанавливает атрибуты для сброса пароля
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(reset_sent_at, Time.zone.now)
   end
 
   private
