ea8452166dc04d72517637abcb002d401af7ceae
MacBook Air 13 <ror.web_developer@yahoo.com>
2021-02-11 21:21:30 +0300

    Update action to change the password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,7 @@
 class PasswordResetsController < ApplicationController
   before_action :get_user, only: [:edit, :update]
   before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
 
   def new
   end
@@ -22,8 +23,27 @@ class PasswordResetsController < ApplicationController
   def edit
   end
 
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "Не может быть пустым.")
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      log_in @user
+      flash[:success] = "Пароль был сброшен."
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
+  def user_params
+    params.require(:user).permit(:password,
+                                 :password_confirmation)
+  end
+
+  # Предфильтры
   def get_user
     @user = User.find_by(email: params[:email])
   end
@@ -35,5 +55,13 @@ class PasswordResetsController < ApplicationController
       redirect_to root_url
     end
   end
-  
+
+  # Проверяет срок действия reset-токена
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:danger] = "Password reset has expired."
+      redirect_to new_password_reset_url
+    end
+  end
+
 end
